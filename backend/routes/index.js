20e1800ebfb138162c00ba6f44d182dca80c6944
christopher <christopher.anthony.dobson@gmail.com>
2022-10-01 15:42:26 -0400

    complete phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/hello/world', function (req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
 
 // backend/routes/index.js
 // ...
